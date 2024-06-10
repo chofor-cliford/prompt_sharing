4b4ce85a3e5fd1be83395b5c49bdc46c4be0e29d
Chofor <betterfuture032@gmail.com>
2024-06-10 15:33:34 -0600

    implement main section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,28 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      boxShadow: {
+        "custom-inset": "inset 10px -50px 94px 0 rgba(199, 199, 199, 0.2)",
+      },
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+        "satoshi-light": ["Satoshi-Light", "sans-serif"],
+        "satoshi-light-italic": ["Satoshi-LightItalic", "sans-serif"],
+        "satoshi-regular": ["Satoshi-Regular", "sans-serif"],
+        "satoshi-italic": ["Satoshi-Italic", "sans-serif"],
+        "satoshi-medium": ["Satoshi-Medium", "sans-serif"],
+        "satoshi-medium-italic": ["Satoshi-MediumItalic", "sans-serif"],
+        "satoshi-bold": ["Satoshi-Bold", "sans-serif"],
+        "satoshi-bold-italic": ["Satoshi-BoldItalic", "sans-serif"],
+        "satoshi-black": ["Satoshi-Black", "sans-serif"],
+        "satoshi-black-italic": ["Satoshi-BlackItalic", "sans-serif"],
+        "satoshi-variable": ["Satoshi-Variable", "sans-serif"],
+        "satoshi-variable-italic": ["Satoshi-VariableItalic", "sans-serif"],
+        inter: ["var(--font-inter)", "sans-serif"],
+      },
+
+      colors: {
+        "primary-orange": "#FF5722",
       },
     },
   },
